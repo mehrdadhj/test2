f80911a1a96507a0954f774016c0b12008d2ce02
mehrdad hojati <mehrdad.hojati@outlook.com>
2021-11-29 17:33:38 +0330

    Merge with framework

--- tools/services/corp.service.ts
+++ tools/services/corp.service.ts
@@ -1,5 +1,19 @@
 export class CorpService {
     private data: string[];
+<<<<<<< HEAD
+
+    constructor() {
+        this.data = this.callApi();
+    }
+
+    private callApi(): string[] {
+        this.data = [];
+        //call api
+        return this.data;
+    }
+
+    public GetData(): string[] {
+=======
     CallApi(): string[] {
         //call api
         this.data = [];
@@ -7,6 +21,7 @@ export class CorpService {
     }
 
     GetData(): string[] {
+>>>>>>> 0ff431b70bdc47838a6f37c6348a51fe62a26bf5
         return this.data;
     }
 
